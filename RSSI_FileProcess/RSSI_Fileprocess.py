9863774e3121908c610971390332d41ca8316142
iuysys <iuysys@163.com>
2019-09-26 19:42:33 +0800

    创建仓库

---
+++ RSSI_FileProcess/RSSI_Fileprocess.py
@@ -0,0 +1,37 @@
+Str_Left = 'LEFT_NODE:'
+Str_Right = 'RIGHT_NODE:'
+Str_Master = 'MASTER_NODE:'
+Str_Rear = 'REAR_NODE:'
+
+FileDirector = "F:\PythonProject\RSSI_FileProcess\\"            # 更改为用户选择路径
+FileNameTemp = 'data01.TXT'
+
+
+def HexList2DecList(HexList):
+    HexListSplit = HexList.strip().split()
+    DecList = list()
+
+    for HexStr in HexListSplit:
+        DecList.append(bytes.fromhex(HexStr))
+
+    return DecList
+
+
+RssiFile = open(FileDirector + FileNameTemp)
+OutPutFile = open(FileDirector + "OutPutFile.txt", 'w')
+LineBuff = list()
+
+for line in RssiFile:
+    if line.find(Str_Left) >= 0:
+        print(line)
+        LineBuff.append(line)
+    elif line.find(Str_Right) >= 0:
+        print(line)
+        LineBuff.append(line)
+    elif line.find(Str_Master) >= 0:
+        print(line)
+        LineBuff.append(line)
+    elif line.find(Str_Rear) >= 0:
+        print(line)
+        LineBuff.append(line)
+OutPutFile.write(LineBuff)
