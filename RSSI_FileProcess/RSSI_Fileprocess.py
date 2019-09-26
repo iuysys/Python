1e992f3c17273d788aaf7b60750aacb9420da3ea
iuysys <iuysys@163.com>
2019-09-26 21:47:49 +0800

    基础可用

--- RSSI_FileProcess/RSSI_Fileprocess.py
+++ RSSI_FileProcess/RSSI_Fileprocess.py
@@ -1,37 +1,61 @@
+# ================================================================
+# -- Import
+# ================================================================
+from matplotlib import pyplot as plt
+
+
+# ================================================================
+# -- Const
+# ================================================================
 Str_Left = 'LEFT_NODE:'
 Str_Right = 'RIGHT_NODE:'
 Str_Master = 'MASTER_NODE:'
 Str_Rear = 'REAR_NODE:'
-
-FileDirector = "F:\PythonProject\RSSI_FileProcess\\"            # 更改为用户选择路径
+# ================================================================
+# -- Variable
+# ================================================================
+FileDirector = "F:\PythonProject\RSSI_FileProcess\\"  # 更改为用户选择路径
 FileNameTemp = 'data01.TXT'
+
+LeftRawBuff = []
+
 
 
 def HexList2DecList(HexList):
     HexListSplit = HexList.strip().split()
-    DecList = list()
+    DecList = []
 
     for HexStr in HexListSplit:
-        DecList.append(bytes.fromhex(HexStr))
+        DecList.append(int(HexStr, 16) - 256)
 
     return DecList
 
 
 RssiFile = open(FileDirector + FileNameTemp)
 OutPutFile = open(FileDirector + "OutPutFile.txt", 'w')
-LineBuff = list()
+LineBuff = []
 
 for line in RssiFile:
     if line.find(Str_Left) >= 0:
         print(line)
+        LeftRawBuff += HexList2DecList(line[len(Str_Left):])
         LineBuff.append(line)
     elif line.find(Str_Right) >= 0:
         print(line)
+        print(HexList2DecList(line[len(Str_Right):]))
         LineBuff.append(line)
     elif line.find(Str_Master) >= 0:
         print(line)
+        print(HexList2DecList(line[len(Str_Master):]))
         LineBuff.append(line)
     elif line.find(Str_Rear) >= 0:
         print(line)
+        print(HexList2DecList(line[len(Str_Rear):]))
         LineBuff.append(line)
+
 OutPutFile.writelines(LineBuff)
+
+xx = range(0, len(LeftRawBuff))
+plt.plot(xx,LeftRawBuff)
+plt.gca().invert_yaxis()
+plt.show()
